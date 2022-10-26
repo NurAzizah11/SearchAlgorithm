1d0820a5bfa7fb8baac3b54dfe6b7e63ff61800a
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-10-26 16:06:56 +0700

    Add project files.

---
+++ SearchAlgorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+
+        public void input()
+        {
+
+        }
+        static void 
+    }
+}
