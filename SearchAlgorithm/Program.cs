f6e399df0cfa7460474c11410b4ac40d877e210d
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-10-26 16:24:02 +0700

    menambahkan perungalan while

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -12,11 +12,51 @@ namespace SearchAlgorithm
         int[] arr = new int[20];
         //Number of elements in the array
         int n;
+        int i;
 
         public void input()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array;");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine
+                        ("\nArray should have minimun 1 and maximum 20 elements. \n");
+            }
+            //Accept array elements 
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" Enter array elements");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                // accept the number to be searched
+                Console.Write
+                    ("\nEnter element want you to search ;");
+            int item = Convert.ToInt32
+
+            }
+        }
+
+
+        
+
 
         }
-        static void 
+        static void Main(string[] args)
     }
 }
